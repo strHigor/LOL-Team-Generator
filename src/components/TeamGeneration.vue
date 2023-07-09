a60075911da1f47ea378e3ae2943831a844a7291
Higor <higor.b97@hotmail.com>
2023-07-09 20:35:06 -0300

    feat: adiciona as fontes aos textos dos participantes e campeoes

--- src/components/TeamGeneration.vue
+++ src/components/TeamGeneration.vue
@@ -15,7 +15,7 @@
 
                 <div id="blueTeamParticipants">
                     <div class="blueTeam" v-for="participant in teams[0]">
-                        {{ participant.toUpperCase() }}
+                        <h5>{{ participant.toUpperCase() }}</h5>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
             <div id="team2" v-if="champions.length > 0 && teams.length > 0">
                 <div id="redTeamParticipants">
                     <div class="redTeam" v-for="participant in teams[1]">
-                        {{ participant.toUpperCase() }}
+                       <h5> {{ participant.toUpperCase() }} </h5>
                     </div>
                 </div>
 
@@ -74,6 +74,22 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+    font-family: 'Lol Fonte';
+    src: url('../fonts/Friz Quadrata Regular.ttf') format('truetype'),
+        url('../fonts/Friz Quadrata Std Medium.otf') format('opentype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+h5 {
+    font-family: 'Lol Fonte', monospace;
+    font-weight: normal;
+    color: #fdd54d;
+    text-shadow: 2px 0 #60542c, -2px 0 #60542c, 0 2px #60542c, 0 -2px #60542c,
+        1px 1px #60542c, -1px -1px #60542c, 1px -1px #60542c, -1px 1px #60542c;
+}
+
 img {
     box-shadow: 0px 0px 3px 2px rgba(0, 0, 0, 0.51);
 }
@@ -92,7 +108,7 @@ img {
 
 .blueTeam {
     color: #ffffff;
-    font-weight: bold;
+    font-size: 20px;
     margin-left: 5px;
     padding: 5px;
 }
@@ -127,7 +143,7 @@ img {
 
 .redTeam {
     color: #ffffff;
-    font-weight: bold;
+    font-size: 20px;
     margin-left: 5px;
     padding: 5px;
 }
